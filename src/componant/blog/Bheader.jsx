0e6e213f081951a5cfcafc7b9d8ab114de533df3
abdalrahmanabase <abdalrahmanabase@gmail.com>
2025-03-18 01:18:15 +0200

    ,

--- src/componant/blog/Bheader.jsx
+++ src/componant/blog/Bheader.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Bheader.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Bheader.css";
 
-const Bheader=()=> {
-return (
-    <div className='blogheader'>
-        <div className='bloghead'>
-            <img src="/imgs/Group 55blog.png" alt='...'></img>
-            <p><Link to='/'>Home</Link> <i class="fa-solid fa-angle-right"></i>  <Link to={location.pathname}>Blog</Link> </p>
+const Bheader = () => {
+
+    return (
+        <div className="blogheader">
+            <div className="bloghead">
+                <img src="/imgs/Group 55blog.png" alt="Blog Header" />
+                <p>
+                    <Link to="/">Home</Link> 
+                    <i className="fa-solid fa-angle-right"></i>    
+                    <Link to={location.pathname}>Blog</Link>
+                </p>
+            </div>
         </div>
-    </div>
-)
-}
+    );
+};
 
-export default Bheader
+export default Bheader;
