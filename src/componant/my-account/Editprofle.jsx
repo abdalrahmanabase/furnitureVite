de9437f9e222905446e9c1954f8e073200f5a29e
abdalrahmanabase <abdalrahmanabase@gmail.com>
2025-03-18 02:02:28 +0200

    .

--- src/componant/my-account/Editprofle.jsx
+++ src/componant/my-account/Editprofle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from '../Footer'
 import Navbar from "../Navbar"
 import Mheader from './Mheader';
-import Ssection2 from '../Shop/Ssection2';
+import Ssection2 from '../shop/Ssection2';
 const Editprofle = () => {
   return (
     <div>
